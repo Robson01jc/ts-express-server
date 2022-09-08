688555a41dd460a42b192469a3a59c23dcb4b0ff
Robson Carvalho <robson01jc@gmail.com>
2022-09-08 20:55:27 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
